fb5fbb3c5e70ba83a8b976170c4d7ee16e927bfb
samah anwer alsatary <alsatary.samah@yahoo.com>
2022-06-28 21:55:17 +0300

    messageQueue

--- piolet/piolt.js
+++ piolet/piolt.js
@@ -1,28 +1,34 @@
-
-'use strict';
-require('dotenv').config();
-const io = require('socket.io-client');
-let PORT =process.env.PORT;
+"use strict";
+require("dotenv").config();
+const io = require("socket.io-client");
+let PORT = process.env.PORT;
 let host = `http://localhost:${PORT}`;
 // const healthConnection = io.connect(`${host}/health-system`);
-let pioletConnection =io.connect(`${host}/airline`);
-let pioletConnectionBase =io.connect(`${host}`);
+let pioletConnection = io.connect(`${host}/airline`);
+let pioletConnectionBase = io.connect(`${host}`);
 
 console.log(`piolt listining on ${PORT}`);
 
-pioletConnectionBase.on("new-flight",arriveHandler);
-function  arriveHandler(payload){
-    payload.events="arrived";
-    console.log(` Pilot: flight with ID ${payload.Details.flightID} has arrived`);
-    pioletConnectionBase.emit("flightArrived",payload);
+pioletConnectionBase.emit("get_all");
+///////////////new-flight//////////////////////////
+pioletConnectionBase.on("new-flight", arriveHandler);
+function arriveHandler(payload) {
+  payload.events = "arrived";
+  console.log(` Pilot: flight with ID ${payload.Details.flightID} has arrived`);
+  pioletConnectionBase.emit("flightArrived", payload);
+ 
 }
-
-
-
+////////////////flight///////////////////////////////
+pioletConnectionBase.on("flight", (payload) => {
+  console.log("piolt base")
+  // console.log(payload)
+  console.log(    `Pilot:Sorry i didn't catch this flight ID ${payload.payload.Details.flightID}`  );
+  pioletConnectionBase.emit("delete",payload)
+});
 
 ////////////////////////took off////////////////////////////////////////////
-pioletConnection.on('took-off',tookOffHandler);
-function  tookOffHandler(payload){
-    payload.event="took-off";
-    console.log(` Pilot: flight with ID ${payload.Details.flightID} took-off`)
+pioletConnection.on("took-off", tookOffHandler);
+function tookOffHandler(payload) {
+  payload.event = "took-off";
+  console.log(` Pilot: flight with ID ${payload.Details.flightID} took-off`);
 }
